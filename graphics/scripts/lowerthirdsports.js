63f5a878fdd6649c791d2822dbe5b4c508711b63
Jason Chua <jasonc1804@gmail.com>
2017-11-24 01:58:03 -0500

    Added countdown clock (wip)

--- graphics/scripts/lowerthirdsports.js
+++ graphics/scripts/lowerthirdsports.js
@@ -2,11 +2,15 @@ const player1 			= nodecg.Replicant("player1", 			{defaultValue: "P1"});
 const player1color 	= nodecg.Replicant("player1color", 	{defaultValue: "000000"});
 const player2 			= nodecg.Replicant("player2", 			{defaultValue: "P2"});
 const player2color 	= nodecg.Replicant("player2color", 	{defaultValue: "000000"});
-const score1 				= nodecg.Replicant("score1", 				{defaultValue: "S1"});
-const score2 				= nodecg.Replicant("score2", 				{defaultValue: "S2"});
+const score1 				= nodecg.Replicant("score1", 				{defaultValue: "999"});
+const score2 				= nodecg.Replicant("score2", 				{defaultValue: "999"});
+const clock 				= nodecg.Replicant("clock", 				{defaultValue: "00:00"});
 //document.getElementById("s1").innerHTML = "";
 
 nodecg.listenFor('score_update', function () {
   document.getElementById("score1").innerHTML = score1.value;
 	document.getElementById("score2").innerHTML = score2.value;
+});
+nodecg.listenFor('clock_update', function () {
+  document.getElementById("clock").innerHTML = clock.value;
 });
